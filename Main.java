720fe7b6c01466aa5cb69ddbbbb22d6ad3ecb576
Yukichi <naginoyukichi@gmail.com>
2023-08-09 14:46:29 +0900

    first java

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main{
+  public static void main(String[] args) {
+    System.out.println(255); //10進数
+    System.out.println(0b11111111); //2進数
+    System.out.println(0377); //8進数
+    System.out.println(0xff); //16進数
+    System.out.println(12.33); //浮動小数点数
+    System.out.println(3e4); //指数
+    System.println('A'); //1文字
+    System.println('¥u3012'); //Unicode
+    System.println("Hello"); //文字列
+    System.println(true); //論理値
+  }
+}
