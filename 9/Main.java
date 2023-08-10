3d6ee7ffa99d2d4f3f0ea32563d63a078c45a22b
Yukichi <naginoyukichi@gmail.com>
2023-08-10 16:39:51 +0900

    java practice

---
+++ 9/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+    char[] c;
+    c = new char[3];
+    int[] i = new int[3];
+    String str[] = {"Welcome ", "to ", "Java. "};
+    c[0] = 'A'; c[1] = 'B'; c[2] = 'C';
+    i[0] = 100; i[1] = 200; i[2] = 300;
+    System.out.println("str: " + str.length);
+    System.out.println("c[0] : " + c[0]);
+    Sytem.out.pritnln("i[1] : " + i[1]);
+  }
+}
